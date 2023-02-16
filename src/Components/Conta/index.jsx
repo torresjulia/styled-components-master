202ea8cc7e01795445bfa9a6168b257207e62c73
Júlia Torres <juliadittoro@yahoo.com>
2023-02-15 22:14:37 -0300

    First commit

---
+++ src/Components/Conta/index.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+import privado from "../../assets/images/privado.svg";
+import olho_icone from "../../assets/images/olho.svg";
+import dinheiro from "../../assets/images/dinheiro.svg";
+import { Icone, Box, Detalhe, Saldo, Botao, IconeTema } from "../UI";
+
+import styled from "styled-components";
+
+const Conta = () => {
+  const [toggleState, untoggle] = useState(true);
+
+  const IconeMargin = styled(Icone)`
+    margin-top: 2px;
+  `;
+
+  const toggleHandler = () => {
+    untoggle((toggleState) => !toggleState);
+  };
+
+  return (
+    <Box>
+      <h2>Conta</h2>
+      <div style={{ fontSize: "26px", padding: "20px 0" }}>
+        Saldo disponível{" "}
+        <span>
+          <IconeTema src={dinheiro} alt="Ícone Saldo" />
+        </span>
+        {toggleState ? (
+          <Saldo>
+            <Detalhe>R$</Detalhe> 0,00{" "}
+          </Saldo>
+        ) : null}
+      </div>
+
+      <Botao onClick={toggleHandler}>
+        <IconeMargin
+          src={toggleState ? privado : olho_icone}
+          alt="Privacidade do Saldo"
+        />
+      </Botao>
+    </Box>
+  );
+};
+
+export default Conta;
